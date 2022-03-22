0ffbfd921f2d99e6828025523aad88dcb94fd72a
ytw0728 <ytw0728@woowahan.com>
2022-03-23 01:31:16 +0900

    컨벤션 수정

--- src/pages/api/send.ts
+++ src/pages/api/send.ts
@@ -3,8 +3,8 @@ import * as TheCamp from 'the-camp-lib';
 
 
 process.on('uncaughtException', function(err) {
-    console.log(err);
-});
+    console.log(err)
+})
 
 const soldier = new TheCamp.Soldier(
     process.env.SOLDIER_NAME ?? '',
@@ -19,19 +19,19 @@ const soldier = new TheCamp.Soldier(
 const client = new TheCamp.Client();
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    let {name, title, content} = req.body as {name: string; title: string; content: string;};
+    let {name, title, content} = req.body as { name: string; title: string; content: string }
     if (
         typeof name !== 'string' || name.length < 3 ||
         typeof title !== 'string' || title.length < 10 ||
         typeof content !== 'string' || content.length < 10
     ) {
-        res.status(400);
-        res.end();
-        return;
+        res.status(400)
+        res.end()
+        return
     }
     try {
-        await client.login(process.env.EMAIL ?? '', process.env.PASSWORD ?? '');
-        await client.addSoldier(soldier);
+        await client.login(process.env.EMAIL ?? '', process.env.PASSWORD ?? '')
+        await client.addSoldier(soldier)
     } catch (e) {
         res.setHeader('Error-Point', 'add soldier')
         res.status(500).send(JSON.stringify(e))
@@ -47,12 +47,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         content.replace(/\r/g, '\n');
         content.replace(/\n/g, '<br/>');
 
-        title = title.length > 15 ? `${title.slice(0,15)}...` : title;
+        title = title.length > 15 ? `${title.slice(0,15)}...` : title
 
-        const messageList = [];
-        const header = (idx: number): string => `[${`00${idx}`.slice(-2)}] ${title} (${name}) | `;
-        const headerLen = header(0).length;
-        const contentWindow = 1500 - headerLen;
+        const messageList = []
+        const header = (idx: number): string => `[${`00${idx}`.slice(-2)}] ${title} (${name}) | `
+        const headerLen = header(0).length
+        const contentWindow = 1500 - headerLen
         for (let index = 1; content.length >= contentWindow * (index - 1); index++){
             messageList.push(new TheCamp.Message(
                 title,
@@ -60,15 +60,15 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
                     content.slice(contentWindow * (index - 1), contentWindow * index - 1)
                 ),
                 trainee,
-            ));
+            ))
         }
         for (let message of messageList) {
-            await client.sendMessage(soldier, message);
+            await client.sendMessage(soldier, message)
         }
-        res.status(200).json({done: true});
+        res.status(200).json({done: true})
     } catch (e) {
         res.setHeader('Error-Point', 'send message')
-        res.status(500).send(JSON.stringify(e));
-        res.end();
+        res.status(500).send(JSON.stringify(e))
+        res.end()
     }
 }
