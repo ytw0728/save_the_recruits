1382d3fb0df8126d3fa1ea3d7d1d3f8077aba569
ytw0728 <ytw0728@woowahan.com>
2022-03-23 06:25:21 +0900

    동작 확인 (에러 수정

--- README.md
+++ README.md
@@ -23,6 +23,9 @@ A `type-check` script is also added to `package.json`, which runs TypeScript's `
 ### 환경 변수
 
 ```bash
+EMAIL : '더캠프이메일'
+PASSWORD : '더캠프 비밀번호'
+
 SOLDIER_NAME : '이름'
 SOLDIER_BIRTHDAY : '생일' // ex 19980728
 SOLDIER_ENTER_DATE : '입소일'// ex 20201008
@@ -30,14 +33,11 @@ SOLDIER_CLASS_NAME : '성분' // ex 예비군인/훈련병
 SOLDIER_GROUP_NAME : '군종' // ex 육군
 SOLDIER_UNIT_NAME : '연대' // ex 육군훈련소(25연대)
 
-
-// 직접 테스트해서 network에서 따오기
-TRAIN_UNIT_CD= '입영부대 코드'
-TRAIN_UNIT_EDU_SEQ=14115
-TRAINEE_MGR_SEQ = '훈련병 식별 코드' 
-
-EMAIL : '더캠프이메일'
-PASSWORD : '더캠프 비밀번호'
+// 직접 위문편지 쓰기 메뉴, 카페 목록 메뉴의 console에서 따오기
+TRAIN_UNIT_TYPE_CD = '부대타입'
+TRAIN_UNIT_CD= '입영부대 코드(교육부대 코드)'
+TRAIN_UNIT_EDU_SEQ='교육대 코드'
+TRAINEE_MGR_SEQ = '훈련병 식별 코드'
 
 NEXT_PUBLIC_NAME : '페이지에 표시될 이름' // 윤태원
 NEXT_PUBLIC_BIRTHDAY : '페이지에 표시될 출생 날짜' // 07/28/1998 출생
--- src/pages/api/send.ts
+++ src/pages/api/send.ts
@@ -1,16 +1,24 @@
-import axios, { AxiosInstance } from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
-
+import * as TheCamp from 'the-camp-lib'
 
 process.on('uncaughtException', function(err) {
     console.log(err)
 })
 
+const id = process.env.EMAIL || '';
+const password = process.env.PASSWORD || '';
+
+const NAME = process.env.SOLDIER_NAME || '';
+const BIRTHDAY = process.env.SOLDIER_BIRTHDAY || '';
+const ENTERDATE = process.env.SOLDIER_ENTER_DATE || '';
+const CLASSNAME = process.env.SOLDIER_CLASS_NAME || '';
+const GROUPNAME = process.env.SOLDIER_GROUP_NAME || '';
+const UNITNAME = process.env.SOLDIER_UNIT_NAME || '';
+
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     let {name, title, content} = req.body as { name: string; title: string; content: string }
-    const client = axios.create({
-        baseURL: 'https://www.thecamp.or.kr',
-    })
+
     if (
         typeof name !== 'string' || name.length < 3 ||
         typeof title !== 'string' || title.length < 10 ||
@@ -21,109 +29,63 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         return
     }
 
-    let cookies: string = ''
+    const soldier = new TheCamp.Soldier(
+        NAME,
+        BIRTHDAY,
+        ENTERDATE,
+        CLASSNAME as TheCamp.SoldierClassName,
+        GROUPNAME as TheCamp.SoldierGroupName,
+        UNITNAME as TheCamp.SoldierUnitName,
+        TheCamp.SoldierRelationship.SIBLING,
+    )
+
+    let cookies: TheCamp.Cookie
     try {
-        cookies = await fetchLoginSessionKey(client)
-        cookies = await login(client, process.env.EMAIL ?? '', process.env.PASSWORD ?? '', cookies)
-        cookies += ' ' + await fetchSessionKey(client)
+        cookies = await TheCamp.login(id, password)
     } catch (e) {
+        console.log(e)
+        res.setHeader('Error-Point', 'login error')
         res.status(500).send(JSON.stringify(e))
         res.end()
         return
     }
 
+    const trainee = soldier.clone()
+    trainee.setTraineeMgrSeq(process.env.TRAINEE_MGR_SEQ!)
+
+    const messageList = []
     content.replace(/\r\n/g, '\n');
     content.replace(/\r/g, '\n');
     content.replace(/\n/g, '<br/>');
 
     title = title.length > 15 ? `${title.slice(0,15)}...` : title
 
-    const messageList = []
     const header = (idx: number): string => `[${`00${idx}`.slice(-2)}] ${title} (${name}) | `
     const headerLen = header(0).length
     const contentWindow = 1500 - headerLen
     for (let index = 1; content.length >= contentWindow * (index - 1); index++){
         messageList.push(
-            header(index).concat(
-                content.slice(contentWindow * (index - 1), contentWindow * index - 1)
+            new TheCamp.Message(
+                title, 
+                header(index).concat(
+                    content.slice(contentWindow * (index - 1), contentWindow * index - 1)
+                ),
+                trainee
             )
         )
     }
 
     try {
         for (let message of messageList) {
-            await sendMessage(client, cookies, title, message)
+            await TheCamp.sendMessage(cookies, trainee, message)
         }
-        res.status(200).json({done: true})
     } catch (e) {
         console.error(e)
         res.setHeader('Error-Point', 'send message')
         res.status(500).send(JSON.stringify(e))
         res.end()
+        return
     }
-}
 
-async function fetchLoginSessionKey(client: AxiosInstance) {
-    const response = await client.get('https://www.thecamp.or.kr/login/main.do')
-    return (response.headers['set-cookie']?.map((c) => c.split(';')[0]).join('; ') ?? '')
-}
-
-async function login(client: AxiosInstance, email: string, password: string, cookies: string) {
-    const response = await client.post('https://www.thecamp.or.kr/login/loginA.do', new URLSearchParams({
-        state: 'email-login',
-        autoLoginYn: 'N',
-        findPwType: 'pwFind',
-        userId: email,
-        userPwd: password,
-    }), {
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'Cookie': cookies
-        }
-    })
-    return (response.headers['set-cookie']?.map((c) => c.split(';')[0]).join('; ') ?? '') + ';'
-}
-
-async function fetchSessionKey(client: AxiosInstance) {
-    const response = await client.get('https://www.thecamp.or.kr/consolLetter/viewConsolLetterInsert.do')
-    return (response.headers['set-cookie']?.map((c) => c.split(';')[0]).join('; ') ?? '')
-}
-
-async function sendMessage(client: AxiosInstance, cookies: string, title: string, content: string) {
-    return await client.post('https://www.thecamp.or.kr/consolLetter/insertConsolLetterA.do', new URLSearchParams({
-        boardDiv: 'sympathyLetter',
-        tempSaveYn: 'N',
-        sympathyLetterEditorFileGroupSeq: '',
-        fileGroupMgrSeq: '',
-        fileMgrSeq: '',
-        sympathyLetterMgrSeq: '',
-        traineeMgrSeq: process.env.TRAINEE_MGR_SEQ ?? '',
-        sympathyLetterContent: content,
-        trainUnitCd: process.env.TRAIN_UNIT_CD ?? '',
-        trainUnitEduSeq: process.env.TRAIN_UNIT_EDU_SEQ ?? '',
-        sympathyLetterSubject: title,
-    }), {
-        headers: {
-            ...headers,
-            'Content-Type': 'application/x-www/form/urlendecoded',
-            Cookie: cookies,
-        },
-    }).then((res) => {
-        if (res.data.resultCd === '9999') {
-            throw new Error('서비스 비정상처리')
-        }
-        return res
-    })
-}
-const headers = {
-    'origin': 'https://www.thecamp.or.kr',
-    'referer': 'https://www.thecamp.or.kr/consolLetter/viewConsolLetterInsert.do',
-    'sec-ch-ua': `" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"`,
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': "macOS",
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Mac,intosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36',
-    'x-requested-with': 'XMLHttpRequest',
+    res.status(200).json({done: true})
 }
